406870bb6e7dbc665237a1ae58695439fdf35131
Rodrigo Gil <rodrigo2122sil@gmail.com>
2024-01-25 10:58:46 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -4,11 +4,13 @@
  */
 package chess;
 
+import boardgame.BoardException;
+
 /**
  *
  * @author Rodrigo
  */
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
      
     public ChessException(String msg){
         super(msg);
--- src/chess/system/UI.java
+++ src/chess/system/UI.java
@@ -5,7 +5,10 @@
 package chess.system;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 /**
  *
  * @author Rodrigo
@@ -33,7 +36,20 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
     
-    
+    public static ChessPosition readChessPosition(Scanner sc){
+        try{
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+//recortar resultado da leitura na posição 1 e com "Integer.parseInt se realiza a conversão do resultado para inteiro
+            int row = Integer.parseInt (s.substring(1));
+            return new ChessPosition(column,row);
+        }
+        catch(RuntimeException e){
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+        }
+    }
+        
+        
     public static void printBoard(ChessPiece[][] pieces){
         
         for(int i=0; i< pieces.length; i++){
@@ -45,7 +61,7 @@ public class UI {
             
             System.out.println();
         }
-        System.out.print("  a b c d e f g h");
+        System.out.print("    a b c d e f g h");
     }
     
     
